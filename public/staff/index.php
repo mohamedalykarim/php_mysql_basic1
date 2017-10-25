d7eb8191b980c9b64263fb11ebdd274197ee567c
mohamedalykarim <mohamed.aly.karim@gmail.com>
2017-10-25 20:45:41 +0200

    Initial commit

---
+++ public/staff/index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GBI</title>
+</head>
+<body>
+
+<header>
+    <h1>GBI staff Area</h1>
+</header>
+
+<navigation>
+    <ul>
+        <li><a href="index.php">Menu</a> </li>
+    </ul>
+</navigation>
+
+<footer>
+    &copy; <?php echo date("Y");?> Globe Bank
+</footer>
+
+</body>
+</html>
